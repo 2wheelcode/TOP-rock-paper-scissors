a2ae832483cf174a909053bf733e1b251f57c4d0
2wheelcode <bentleyhamm@gmail.com>
2022-04-26 12:10:43 -0600

    Duh!

--- r-p-s.js
+++ r-p-s.js
@@ -2,7 +2,7 @@
 I. create a function computerPlay() {
     return random of R-P-or-S
     * TIP: use console.log() to verify output before moving on
-}    
+}    - CHECK
     Convo with Peter
     const compChoice = ["Rock", "Paper", "Scissors"]
     compChoice = math.Floor (math.Random ()*1) (keep between 1-3)
@@ -17,20 +17,36 @@ const rpsValue = Math.floor(Math.random()*3)
 function computerPlay() { 
     return ["Rock", "Paper", "Scissors"][rpsValue]   
 }
-computerPlay()
-console.log(rpsValue)
-console.log(computerPlay)
+console.log(rpsValue) // = random number - CHECK
+console.log(computerPlay()) // = random number converted to array value - CHECK
 /*
 II. create a function playRockPaperScissors() {
-    i. parameter - playerSelection
-    * NOTE: make player selection case insensitive
+    i. parameter - playerSelection - CHECK
+    * NOTE: make player selection case insensitive - CHECK
     ii. parameter - computerSelection
     iii. return a string with Winner Declaration
     * this is obviously missing:
-    1.if else loop; rock > scissors > paper > rock
- }
+    1. if else loop; rock > scissors > paper > rock
+    2. input from player prompt to start
+ 
 * Going to want to return the results of the game later, as wll add a Graphical Interface
+} */
+playerInput = prompt("Let's play Rock - Paper - Scissors! \n What's your choice?")
+function playerSelection() {
+return playerInput.toLowerCase().replace(/\b(\w)/, s => s.toUpperCase())
+}
+console.log(playerInput) // = player input - CHECK
+console.log(playerSelection()) // = player input converted to Capitalize, case insensitive - CHECK
 
+function playRPS() {
+    
+
+    //return winnerDeclaration
+}
+
+
+
+/*
 III. create a function game() {
     i. call playRockPaperScissors()
     ii. play 5 rounds, keep score, & report winner

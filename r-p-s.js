b94a7ed357a70088a837c2f508cb2d41d6e9dfc5
2wheelcode <bentleyhamm@gmail.com>
2022-04-27 15:51:16 -0600

    Round play complete Hit the wall again. Why can I not think my way through?

--- r-p-s.js
+++ r-p-s.js
@@ -1,5 +1,5 @@
 /* White Board Notes
-I. create a function computerPlay() {
+I. create a function computerPlay() { * Changing this name to computerSelection() for clarity
     return random of R-P-or-S
     * TIP: use console.log() to verify output before moving on
 }    - CHECK
@@ -14,11 +14,11 @@ I. create a function computerPlay() {
     Ok, let's go!
 */
 const rpsValue = Math.floor(Math.random()*3)
-function computerPlay() { 
-    return ["rock", "paper", "scissors"][rpsValue]   
+function computerSelection() { 
+    return ["Rock", "Paper", "Scissors"][rpsValue]   
 }
-console.log(rpsValue) // = random number - CHECK
-console.log(computerPlay()) // = random number converted to array value - CHECK
+//console.log(rpsValue) // = random number - CHECK
+//console.log(computerSelection()) // = random number converted to array value - CHECK
 /*
 II. create a function playRockPaperScissors() {
     i. parameter - playerSelection - CHECK
@@ -31,28 +31,30 @@ II. create a function playRockPaperScissors() {
  
 * Going to want to return the results of the game later, as wll add a Graphical Interface
 } */
-playerInput = prompt("Let's play Rock - Paper - Scissors! \n What's your choice?")
-function playerSelection() {
-return playerInput.toLowerCase()
-}
-console.log(playerInput) // = player input - CHECK
-console.log(playerSelection()) // = player input converted to Capitalize, case insensitive - CHECK - changed to lowercase for simplicity
 
-function playRPS(playerSelection, computerPlay) {
-    if (playerSelection === computerPlay) {
-        return "Tie Game!"
-        } else if (
-            (playerSelection === 'rock' && computerPlay === 'scissors') ||
-            (playerSelection === 'scissors' && computerPlay === 'paper') ||
-            (playerSelection === 'paper' && computerPlay === 'rock')) {
-                return "Man beats machine! You win!" 
-        } else {
-            return "Machine wins, suffer shame."
-        }
-    //return winnerDeclaration
+//playerInput = prompt("Let's play Rock - Paper - Scissors! \n What's your choice?")
+function playerSelection() {
+return playerInput[0].toUpperCase() + playerInput.slice(1).toLowerCase();
 }
-//playRPS()
-console.log(playRPS())
+//console.log(playerInput) // = player input - CHECK
+//console.log(playerSelection()) // = player input converted to Capitalize, case insensitive - CHECK
+
+function playRound(playerSelection, computerSelection) {
+    console.log(playerSelection, computerSelection) // correct arguments being passed into function
+    if (playerSelection === computerSelection) {
+        return `Simple minds think alike, ${playerSelection} It's a DRAW!`
+        } else if (
+            (playerSelection === 'Rock' && computerSelection === 'Scissors') ||
+            (playerSelection === 'Scissors' && computerSelection === 'Paper') ||
+            (playerSelection === 'Paper' && computerSelection === 'Rock')) {
+                return `Man over machine! ${playerSelection} beats ${computerSelection} You Win!` 
+        } else {
+            return `${computerSelection} beats ${playerSelection} When you match wits with a half wit...the Machines win, humans suffer shame.`
+        }
+    //return winnerDeclaration - CHECK - will come back later to see about changing "beats"
+}
+playRound(playerSelection(), computerSelection())
+console.log(playRound(playerSelection(), computerSelection()))
 
 
 /*
@@ -60,8 +62,12 @@ III. create a function game() {
     i. call playRockPaperScissors()
     ii. play 5 rounds, keep score, & report winner
         for loop
-}
+} */
 
+
+
+
+/*
 **
 feel free to rework the functions if wanted, particularly return value to something more useful
 feel free to create more "helper functions" if I think it's useful
